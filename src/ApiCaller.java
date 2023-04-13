cb63bb65b1481bd161dcd7dfa4f23dcde50b7937
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-04-13 17:18:19 +0200

    Finishing needed functionalities from myanimelist API Implemented ApiCaller.update_my_anime_list and ApiCaller.delete_my_anime_list to modify user lists

--- src/ApiCaller.java
+++ src/ApiCaller.java
@@ -17,6 +17,9 @@ public class ApiCaller {
         try {
             ApiCaller caller = new ApiCaller(args[0]);
             System.out.println(caller.get_anime_ranking(16, 70));
+            System.out.println(caller.update_my_anime_list(49854, "completed", false, 9, 12));
+            Thread.sleep(60000);
+            caller.delete_my_anime_list(49854);
         } catch (Exception ignored) {
             ignored.printStackTrace();
         }
@@ -36,16 +39,48 @@ public class ApiCaller {
     }
 
     public HashMap<String, Object> get_anime_ranking(int limit, int offset) throws IOException {
+        String fields = "num_episodes";
         Request request = new Request.Builder()
                 .url(new UrlBuilder()
                         .setUri("anime/ranking")
                         .addParameter("limit", Integer.toString(limit))
                         .addParameter("offset", Integer.toString(offset))
+                        .addParameter("fields", fields)
                         .build())
                 .method("GET", null)
                 .addHeader("Authorization", headers.get("Authorization"))
                 .build();
         Response response = client.newCall(request).execute();
         return new ObjectMapper().readValue(response.body().string(), HashMap.class);
+    }
+
+    public HashMap<String, Object> update_my_anime_list(int id,
+                                                        String status,
+                                                        boolean is_rewatching,
+                                                        int score,
+                                                        int num_watched_episodes) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, String.format("status=%s&is_rewatching=%s&score=%s&num_watched_episodes=%s",
+                                                                        status, is_rewatching, score, num_watched_episodes));
+        Request request = new Request.Builder()
+                .url(new UrlBuilder()
+                        .setUri("anime/" + Integer.toString(id) + "/my_list_status")
+                        .build())
+                .method("PUT", body)
+                .addHeader("Authorization", headers.get("Authorization"))
+                .build();
+        Response response = client.newCall(request).execute();
+        return new ObjectMapper().readValue(response.body().string(), HashMap.class);
+    }
+
+    public void delete_my_anime_list(int id) throws IOException {
+        Request request = new Request.Builder()
+                .url(new UrlBuilder()
+                        .setUri("anime/" + Integer.toString(id) + "/my_list_status")
+                        .build())
+                .method("DELETE", null)
+                .addHeader("Authorization", headers.get("Authorization"))
+                .build();
+        client.newCall(request).execute();
     }
 }
